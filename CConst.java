9852eb8177a44a9e301aa3d2f9e8ced966557043
陈林伟 <brokge@qq.com>
2016-07-29 14:55:06 +0800

    first－commit
    第一次整理提交

---
+++ CConst.java
@@ -0,0 +1,54 @@
+package com.gootrip.util;
+
+public class CConst {
+
+   /**
+   * ÍøÕ¾Í¼Æ¬Ä¿Â¼
+   */
+   public final static String PIC_ROOT = "/pic/";
+
+   /**
+    * ÍøÕ¾Èí¼þÄ¿Â¼
+    */
+
+   public final static String SOFT_ROOT = "/soft/";
+
+   /**
+    * ¸½¼þÎÄ¼þ´æ·ÅµÄ¾ø¶ÔÂ·¾¶
+    */
+
+   public final static String ATTACH_ROOT = "/attach/";
+
+   /**
+    * ¶¯Ì¬³ÌÐòÂ·¾¶
+    */
+
+   public final static String ProgramPath = "/programs/";
+
+
+   /**
+    * ÉÏ´«Í¼Æ¬´óÐ¡ÏÞÖÆ£¬µ¥Î»byte
+    */
+   public final static  long MAX_UPLOAD_PIC_SIZE = 1000 * 1024 * 4;
+   /**
+    * ÉÏ´«ÎÄ¼þ´óÐ¡ÏÞÖÆ£¬µ¥Î»byte
+    */
+   public final static  long MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024 * 8;
+
+   /**
+    * ÉÏ´«Èí¼þ´óÐ¡ÏÞÖÆ£¬µ¥Î»byte
+    */
+   public final static  long MAX_UPLOAD_SOFT_SIZE = 100 * 1024 * 1024 * 8;
+
+   /**
+     * ´íÎóµÇÂ¼´ÎÊý×î¶à3´Î
+     */
+   public final static  int MAX_LOGIN_TIMES = 3;
+   /**
+     * ´íÎóµÇÂ¼3´ÎºóÓÃ»§±»Ëø10·ÖÖÓ
+     */
+   public final static int LOCK_TIME = +10;
+
+   public final static String[] ADMIN = {"admin", "longlob"};
+
+}
